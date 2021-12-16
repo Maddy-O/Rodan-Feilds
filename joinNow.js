7964a8c9ac68e5de7adc5e08435f87f4d412fa1e
Suraj Kumar <50989453+surajmehta999@users.noreply.github.com>
2021-12-16 09:19:09 +0530

    Update joinNow.js

--- joinNow.js
+++ joinNow.js
@@ -1,8 +1,7 @@
 
     document.querySelector('#form').addEventListener("submit",communityAdd)
     var arr =JSON.parse(localStorage.getItem("communityDetails")) || [];
-    function communityAdd(event){
-     event.preventDefault();
+    function communityAdd(){
      var name =document.querySelector('#nameinput').value;
      var lastName =document.querySelector('#lastNameInput').value;
      var language =document.querySelector('#language1').value;
