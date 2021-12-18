32c021e8021d1878d925f83ea51ecd4429c49833
Suraj Kumar <50989453+surajmehta999@users.noreply.github.com>
2021-12-18 15:00:43 +0530

    Update becomeAConsultant.js
    Some Changes

--- becomeAConsultant.js
+++ becomeAConsultant.js
@@ -8,6 +8,27 @@
     function signUp(){
         window.location.href='joinnow.html';
     }
+
+function fb() {
+    window.location.href = 'https://www.facebook.com/rodanandfields';
+}
+
+function twit() {
+    window.location.href = 'https://twitter.com/RodanFieldsHQ';
+}
+
+function insta() {
+    window.location.href = 'https://www.instagram.com/rodanandfields/';
+}
+function you() {
+    window.location.href = 'https://www.youtube.com/user/RodanFields';
+}
+function pinte() {
+    window.location.href = 'https://in.pinterest.com/RodanandFields/';
+}
+
+
+
     var acc = document.getElementsByClassName("accordion");
 var i;
 
